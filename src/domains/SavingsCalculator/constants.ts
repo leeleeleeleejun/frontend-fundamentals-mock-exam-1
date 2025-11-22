a419f363619df3475a70f5cbde68bee1edfa8956
leeleeleeleejun <wnstjr6293@naver.com>
2025-11-22 23:25:26 +0900

    feat: AVAILABLE_TERMS_OPTION에 18개월 추가

--- src/domains/SavingsCalculator/constants.ts
+++ src/domains/SavingsCalculator/constants.ts
@@ -5,5 +5,5 @@ export const SAVINGS_RESULT_TABS = {
 export type SavingsResultTab = keyof typeof SAVINGS_RESULT_TABS;
 export const SavingResultTabsList = Object.keys(SAVINGS_RESULT_TABS) as SavingsResultTab[];
 
-export const AVAILABLE_TERMS_OPTION = [6, 12, 24] as const;
+export const AVAILABLE_TERMS_OPTION = [6, 12, 18, 24] as const;
 export type AvailableTermsOption = (typeof AVAILABLE_TERMS_OPTION)[number];
